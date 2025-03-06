6eb5984b176f1396957d763d54987d6d77af1b87
Amisha Pruseth <amisha_pruseth@epam.com>
2025-03-06 13:05:17 +0530

    commited

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,21 +1,3 @@
 exports.handler = async (event) => {
-    const path = event.rawPath || event.path;
-    const method = event.requestContext?.http?.method || event.httpMethod;
-
-    if (path === "/hello" && method === "GET") {
-        return {
-            statusCode: 200,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({statusCode : 200,  message: "Hello from Lambda" }),
-        };
-    }
-
     return {
-        statusCode: 400,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-        "statusCode": 400,
-        "message": `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
-        }),
-    };
-}
+       statusCode: 200,body: JSON.stringify({statusCode:200, message: "Hello from Lambda" }), // Removed duplicate statusCode        };};
