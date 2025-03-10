272d099a5a05e2ed3bb835755de6971bfe111928
Amisha Pruseth <amisha_pruseth@epam.com>
2025-03-10 16:09:04 +0530

    commited

--- task10/app/lambdas/processor/index.js
+++ task10/app/lambdas/processor/index.js
@@ -8,25 +8,29 @@ const dynamoDB = AWSXRay.captureAWSClient(new AWS.DynamoDB.DocumentClient());
 const tableName = process.env.TABLE_NAME;
 
 // Open-Meteo API URL
-const API_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
+const API_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m)";
 
 exports.handler = async (event) => {
   const segment = AWSXRay.getSegment().addNewSubsegment("WeatherDataProcessing");
+  console.log("Lambda triggered. Fetching weather data...");
 
   try {
     // Fetch weather data from Open-Meteo API
     const weatherResponse = await axios.get(API_URL);
     const weatherData = weatherResponse.data;
+    console.log("Weather data fetched:", JSON.stringify(weatherResponse.data));
+
 
     // Prepare data for DynamoDB
     const item = {
       id: uuidv4(),
-      forecast: weatherData,
+      forecast: weatherResponse.data,
     };
+    console.log("Storing data in DynamoDB:", JSON.stringify(item));
 
     // Store data in DynamoDB
     await dynamoDB.put({ TableName: tableName, Item: item }).promise();
-
+     console.log("Data successfully stored in DynamoDB");
     segment.close();
 
     return {
@@ -36,6 +40,7 @@ exports.handler = async (event) => {
   } catch (error) {
     segment.addError(error);
     segment.close();
+    console.error("Error:", error);
     return { statusCode: 500, body: JSON.stringify({ message: "Error fetching or storing data", error }) };
   }
 };
